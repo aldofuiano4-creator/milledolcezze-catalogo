acd5f4a2c77cbb01fd89b8be2e59284b738688d0
Milledolcezze <aldofuiano4@gmail.com>
2026-07-22 21:18:58 +0200

    Create caramelle.js

---
+++ caramelle.js
@@ -0,0 +1,88 @@
+const caramelle = [
+
+{
+nome: "Orsetti frutta",
+categoria: "Frutta",
+emoji: "🍬"
+},
+
+{
+nome: "Fragole panna",
+categoria: "Frutta",
+emoji: "🍓"
+},
+
+{
+nome: "Pesche",
+categoria: "Frutta",
+emoji: "🍑"
+},
+
+{
+nome: "Angurie",
+categoria: "Frutta",
+emoji: "🍉"
+},
+
+{
+nome: "Ciliegie",
+categoria: "Frutta",
+emoji: "🍒"
+},
+
+{
+nome: "Banane",
+categoria: "Frutta",
+emoji: "🍌"
+},
+
+{
+nome: "Ananas gommose",
+categoria: "Frutta",
+emoji: "🍍"
+},
+
+{
+nome: "Ananas zuccherate",
+categoria: "Frutta",
+emoji: "🍍"
+},
+
+{
+nome: "Coccole panna e fragola rosa e bianche",
+categoria: "Frutta",
+emoji: "🍓"
+},
+
+
+{
+nome: "Bottiglie cola",
+categoria: "Cola",
+emoji: "🥤"
+},
+
+{
+nome: "Coca Cola gommosa",
+categoria: "Cola",
+emoji: "🥤"
+},
+
+{
+nome: "Coca Cola bottiglia zucchero",
+categoria: "Cola",
+emoji: "🥤"
+},
+
+{
+nome: "Ciucci cola frizzante zuccherate",
+categoria: "Cola",
+emoji: "🥤"
+},
+
+{
+nome: "Vermi a coca cola",
+categoria: "Cola",
+emoji: "🐛"
+}
+
+];
