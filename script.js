53cce3b0bf5fb61259de06951e96ee22c8e97d00
Milledolcezze <aldofuiano4@gmail.com>
2026-07-22 20:51:39 +0200

    Add candy box selection and ordering functionality

---
+++ script.js
@@ -0,0 +1,115 @@
+let box = "";
+let limite = 0;
+let scelte = [];
+
+
+function scegliBox(nome, massimo){
+
+    box = nome;
+    limite = massimo;
+    scelte = [];
+
+    document.getElementById("boxScelta").innerHTML =
+    "Candy Box scelta: <b>" + box + "</b>";
+
+    aggiorna();
+
+}
+
+
+
+function aggiungi(caramella){
+
+    if(limite === 0){
+
+        alert("Prima scegli una Candy Box!");
+
+        return;
+    }
+
+
+    if(scelte.length >= limite){
+
+        alert("Hai completato la tua Candy Box!");
+
+        return;
+    }
+
+
+    scelte.push(caramella);
+
+    aggiorna();
+
+}
+
+
+
+function aggiorna(){
+
+    let lista = document.getElementById("lista");
+
+    lista.innerHTML = "";
+
+
+    scelte.forEach(function(item){
+
+        let li = document.createElement("li");
+
+        li.innerHTML = "🍬 " + item;
+
+        lista.appendChild(li);
+
+    });
+
+
+
+    let contatore = document.getElementById("contatore");
+
+
+    if(limite > 0){
+
+        contatore.innerHTML =
+        "Hai scelto " + scelte.length +
+        " gusti su " + limite;
+
+    }
+
+
+}
+
+
+
+function ordina(){
+
+    if(box === ""){
+
+        alert("Scegli prima la Candy Box");
+
+        return;
+
+    }
+
+
+    let messaggio =
+    "Ciao MILLEDOLCEZZE, vorrei ordinare una Candy Box " 
+    + box +
+    ".\n\nLe mie caramelle:\n";
+
+
+    scelte.forEach(function(item){
+
+        messaggio += "- " + item + "\n";
+
+    });
+
+
+    let numero = "INSERISCI_NUMERO_WHATSAPP";
+
+
+    window.open(
+    "https://wa.me/" + numero +
+    "?text=" + encodeURIComponent(messaggio)
+    );
+
+
+}
