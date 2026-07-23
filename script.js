003467b72b090213997c87a07d31a17a9d307999
Milledolcezze <aldofuiano4@gmail.com>
2026-07-23 22:29:58 +0200

    Refactor candy box selection and addition logic

--- script.js
+++ script.js
@@ -9,129 +9,26 @@ function scegliBox(nome, massimo){
     limite = massimo;
     scelte = [];
 
-    document.getElementById("boxScelta").innerHTML =
-    "Candy Box scelta: <b>" + box + "</b>";
+    document.getElementById("boxScelto").innerHTML =
+    "Candy Box scelta: " + box;
 
     aggiorna();
-
 }
 
 
-
-function aggiungi(caramella){
-
-    if(limite === 0){
-
-        alert("Prima scegli una Candy Box!");
-
-        return;
-    }
-
-
-    if(scelte.length >= limite){
-
-        alert("Hai completato la tua Candy Box!");
-
-        return;
-    }
-
-
-    scelte.push(caramella);
-
-    aggiorna();
-
-}
-
-
-
-function aggiorna(){
-
-    let lista = document.getElementById("lista");
-
-    lista.innerHTML = "";
-
-
-    scelte.forEach(function(item){
-
-        let li = document.createElement("li");
-
-        li.innerHTML = "🍬 " + item;
-
-        lista.appendChild(li);
-
-    });
-
-
-
-    let contatore = document.getElementById("contatore");
-
-
-    if(limite > 0){
-
-        contatore.innerHTML =
-        "Hai scelto " + scelte.length +
-        " gusti su " + limite;
-
-    }
-
-
-}
-
-
-
-function ordina(){
+function aggiungi(nome){
 
     if(box === ""){
-
-        alert("Scegli prima la Candy Box");
-
+        alert("Scegli prima una Candy Box");
         return;
-
     }
 
+    if(scelte.length >= limite){
+        alert("Hai raggiunto il limite di gusti per questa box");
+        return;
+    }
 
-    let messaggio =
-    "Ciao MILLEDOLCEZZE, vorrei ordinare una Candy Box " 
-    + box +
-    ".\n\nLe mie caramelle:\n";
+    scelte.push(nome);
 
-
-    scelte.forEach(function(item){
-
-        messaggio += "- " + item + "\n";
-
-    });
-
-
-    let numero = "INSERISCI_NUMERO_WHATSAPP";
-
-
-    window.open(
-    "https://wa.me/" + numero +
-    "?text=" + encodeURIComponent(messaggio)
-    );
-
-
+    aggiorna();
 }
-window.onload = function(){
-
-    let contenitore = document.getElementById("caramelle");
-
-    caramelle.forEach(function(item){
-
-        let bottone = document.createElement("button");
-  bottone.innerHTML =
-"<img src='immagini/" + item.foto + "' width='100'><br>" +
-item.emoji + " " + item.nome + " +";
-
-        bottone.onclick = function(){
-
-            aggiungi(item.nome);
-
-        };
-
-        contenitore.appendChild(bottone);
-
-    });
-
-};
