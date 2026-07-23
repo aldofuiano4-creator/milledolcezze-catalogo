d313fc9dec91c613093de8f9f94a4f9b4802f7dd
Milledolcezze <aldofuiano4@gmail.com>
2026-07-23 22:32:42 +0200

    Add dynamic content loading on window load

--- script.js
+++ script.js
@@ -105,3 +105,39 @@ function ordina(){
     window.open(link, "_blank");
 
 }
+
+window.onload = function(){
+
+    let contenitore = document.getElementById("catalogo");
+
+
+    if(contenitore){
+
+        caramelle.forEach(function(item){
+
+
+            let card = document.createElement("div");
+
+            card.className = "caramella";
+
+
+            card.innerHTML =
+
+            "<img src='images/" + item.foto + "'>" +
+
+            "<h3>" + item.emoji + " " + item.nome + "</h3>" +
+
+
+            "<button onclick=\"aggiungi('" 
+            + item.nome +
+            "')\">Aggiungi</button>";
+
+
+            contenitore.appendChild(card);
+
+
+        });
+
+    }
+
+};
